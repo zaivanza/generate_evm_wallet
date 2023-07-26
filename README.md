195883d4e3a846ac79b3f130cfe4970498447629
hodlmod.eth <zasara1980@gmail.com>
2023-07-26 13:08:03 +0500

    create files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# generate_evm_wallet
+[![Typing SVG](https://readme-typing-svg.herokuapp.com?color=%2336BCF7&lines=generate_evm_wallets)](https://git.io/typing-svg)
+
+Скрипт по созданию evm кошельков. Можно создавать красивые адреса с определенным кол-вом нулей после 0x.
+
+1. Скачиваем библиотеку : pip install hdwallet
+2. Запускаем main.py
+3. В терминале пишем сколько хотим нулей и кол-во кошельков. 
+4. Сид фраза, приватник и адрес будут записаны в txt файлы.
+
+Donate (evm) : `0xb7415DB78c886c67DBfB25D3Eb7fcd496dAf9021` or `donates-for-hodlmod.eth`
+
+Паблик : https://t.me/hodlmodeth. [ code ] чат : https://t.me/code_hodlmodeth. Канал с обновлениями и лайф-рофл-контентом : https://t.me/never_broke_again_v1
---
+++ main.py
@@ -0,0 +1,82 @@
+from typing import Optional
+from hdwallet import BIP44HDWallet
+from hdwallet.cryptocurrencies import EthereumMainnet
+from hdwallet.derivations import BIP44Derivation
+from hdwallet.utils import generate_mnemonic
+import time
+
+target_zeros = int(input('\nEnter number of zeros after "0x" : ')) # Number of 0 after 0x
+n_wallets = int(input("Enter number of wallets : ")) # Number of wallets
+print()
+
+def generate_evm_wallet(target_zeros, n_wallets):
+
+    wallets = {
+        'mnemonics' : [],
+        'addresses' : [],
+        'keys' : [],
+    }
+
+    for i in range(n_wallets):
+
+        start = time.perf_counter()
+
+        while True:
+
+            # Generate english mnemonic words
+            MNEMONIC: str = generate_mnemonic(language="english", strength=128)
+            # Secret passphrase/password for mnemonic
+            PASSPHRASE: Optional[str] = None  # "meherett"
+
+            # Initialize Ethereum mainnet BIP44HDWallet
+            bip44_hdwallet: BIP44HDWallet = BIP44HDWallet(cryptocurrency=EthereumMainnet)
+            # Get Ethereum BIP44HDWallet from mnemonic
+            bip44_hdwallet.from_mnemonic(
+                mnemonic=MNEMONIC, language="english", passphrase=PASSPHRASE
+            )
+            # Clean default BIP44 derivation indexes/paths
+            bip44_hdwallet.clean_derivation()
+            mnemonics = bip44_hdwallet.mnemonic()
+
+            # Get Ethereum BIP44HDWallet information's from address index
+
+            # Derivation from Ethereum BIP44 derivation path
+            bip44_derivation: BIP44Derivation = BIP44Derivation(
+                cryptocurrency=EthereumMainnet, account=0, change=False, address=0
+            )
+            # Drive Ethereum BIP44HDWallet
+            bip44_hdwallet.from_path(path=bip44_derivation)
+            # Print address_index, path, address and private_key
+            address = bip44_hdwallet.address()
+            key = bip44_hdwallet.private_key()
+            # Clean derivation indexes/paths
+            bip44_hdwallet.clean_derivation() 
+
+            if address.startswith("0x" + "0" * target_zeros):
+                print(f'{address} : {int((time.perf_counter() - start))} sec.')
+                wallets["mnemonics"].append(mnemonics)
+                wallets["addresses"].append(address)
+                wallets["keys"].append(f'{key}')
+                break
+
+    file_keys = open(f"key.txt", "w")
+    file_addr = open(f"address.txt", "w")
+    file_seed = open(f"seed.txt", "w")
+
+    for x in wallets["mnemonics"]:
+        file_seed.write(f'{str(x)}\n')
+
+    for x in wallets["addresses"]:
+        file_addr.write(f'{str(x)}\n')
+
+    for x in wallets["keys"]:
+        file_keys.write(f'0x{str(x)}\n')
+
+    file_keys.close()
+    file_addr.close()
+    file_seed.close()
+
+    print(f'\nThe result is written to key.txt address.txt seed.txt')
+
+if __name__ == "__main__":
+    generate_evm_wallet(target_zeros, n_wallets)
